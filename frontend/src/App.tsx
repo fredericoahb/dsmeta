5648f38b77b718418f4678507c875024825deb69
Frederico Balieiro <fredericoahb@gmail.com>
2022-11-06 17:07:56 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,21 @@
-import NotificationButton from "./components/NotificationButton"
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
+import logo from '../../assets/img/logo-dsmeta.svg';
+import './style.css';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <section id="sales">
-          <div className="dsmeta-container">
-            <SalesCard />
-          </div>
-        </section>
-      </main>
-    </>
-  )
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/joiamarketing/" target="_blank">
+                        @joiamarketing
+                    </a>
+                </p>
+            </div>
+        </header>
+    )
 }
 
-export default App
+export default Header;
