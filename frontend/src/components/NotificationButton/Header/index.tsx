7a25309a63a105e3cc61449a775c43969c081bd6
Frederico Balieiro <fredericoahb@gmail.com>
2022-11-06 16:50:00 -0300

    Datepicker

---
+++ frontend/src/components/NotificationButton/Header/index.tsx
@@ -0,0 +1,21 @@
+import logo from '../../assets/img/logo-dsmeta.svg'
+import './style.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/joiamarketing/" target="_blank">
+                        @joiamarketing
+                    </a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
