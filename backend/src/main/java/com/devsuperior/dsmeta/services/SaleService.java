f73f0dc2c65e35698c1d2dee349d683ce87420ab
Frederico Balieiro <fredericoahb@gmail.com>
2022-11-07 17:19:34 -0300

    Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -15,7 +19,16 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		//COLOCAR DATA DEFOULT 
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		//CONVERTER A data NA FORMA DE TEXTO PARA LocalDate
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+		//O JPA NÃO UMA FUNÇÃO PARA PROCESSAR DUAS DATAS, PRECISA INCLUIR ISSO NO repositories
+		return repository.findAll(min, max, pageable);
 	}
 }
